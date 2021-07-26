5d41d34e4fd4918bce3ca795c204d9deb5d8c485
Timur Shmateyev <83357176+TimurShmateyev@users.noreply.github.com>
2021-07-26 18:01:45 +0500

    Update posts.php

--- posts.php
+++ posts.php
@@ -1,10 +1,14 @@
 <?php
-$content = preg_replace(
-    '#<!-- posts -->.*<!-- /posts -->#s',
-    '<!-- posts -->Hi<!-- /posts -->',
-    file_get_contents('README.md')
-);
+date_default_timezone_set('GMT+5');
+$curentTimestamp = time();
+$startTimestamp = '1577836800';
+settype($curentTimestamp, 'integer');
+settype($startTimestamp, 'integer');
+$diffense = $curentTimestamp - $startTimestamp;
+settype($diffense, 'string');
+$date = date('Y-m-d H:i', $diffense);
+$content = $date;
 
 // Overwrite the file
-//file_put_contents('README.md', $content);
+file_put_contents('README.md', $content);
 ?>
