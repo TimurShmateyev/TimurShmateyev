c7ddc4cdcba32c3eb562fad48d130876318f013e
Timur Shmateyev <83357176+TimurShmateyev@users.noreply.github.com>
2021-07-26 22:01:41 +0500

    Update posts.php

--- posts.php
+++ posts.php
@@ -9,7 +9,7 @@ settype($diffense, 'string');
 $date = date('m-d H:i', $diffense);
 $content = preg_replace(
     '#<!-- posts -->.*<!-- /posts -->#s',
-    '#####<!-- posts -->'.$date.'<!-- /posts -->',
+    '<bold><!-- posts -->'.$date.'<!-- /posts --></bold>',
     file_get_contents('README.md')
 );
 
