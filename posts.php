3c8f81beb596d50b59e5ba1bc08713cd722a22ba
Timur Shmateyev <83357176+TimurShmateyev@users.noreply.github.com>
2021-07-26 18:17:00 +0500

    Update posts.php

--- posts.php
+++ posts.php
@@ -6,7 +6,7 @@ settype($curentTimestamp, 'integer');
 settype($startTimestamp, 'integer');
 $diffense = $curentTimestamp - $startTimestamp;
 settype($diffense, 'string');
-$date = date('Y-m-d H:i', $diffense);
+$date = date('m-d H:i', $diffense);
 $content = preg_replace(
     '#<!-- posts -->.*<!-- /posts -->#s',
     '<!-- posts -->'.$date.'<!-- /posts -->',
