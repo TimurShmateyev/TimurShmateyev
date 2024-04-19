9b553f6d90ff2d48141334e122cec4f060aa01cc
Timur Shmateyev <83357176+TimurShmateyev@users.noreply.github.com>
2024-04-19 11:57:31 +0500

    Update time_updater.php

--- time_updater.php
+++ time_updater.php
@@ -1,7 +1,15 @@
 <?php
+
+$current_timestamp = time();
+$start_timestamp = 1579243800;
+
+$diff = $current_timestamp - $start_timestamp;
+
+$datetime_diff = date('Y-m-d', $diff);
+
 $content = preg_replace(
     '#<!-- posts -->.*<!-- /posts -->#s',
-    '#<!-- posts -->BRUH TIME<!-- /posts -->#s',
+    '#<!-- posts -->'.$datetime_diff.'<!-- /posts -->#s',
     file_get_contents('README.md')
 );
 
